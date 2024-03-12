890761ad9b9cc448d20bb47546986e153106e663
Somtime <rghee0821@gmai.com>
2024-03-12 12:02:36 +0900

    Transaction Decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TransactionInterceptor } from './interceptors/transaction.interceptor';
+import { TransactionManager } from './decorators/transaction.decorator';
+import { EntityManager } from 'typeorm';
 
 @Controller()
 export class AppController {
@@ -8,5 +11,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('transaction')
+  @UseInterceptors(TransactionInterceptor)
+  async transactionDefault(
+    @TransactionManager() transactionManager: EntityManager,
+  ) {
+    return await this.appService.transactionDefault(transactionManager);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  transactionDefault(transactionManager: EntityManager) {
+    throw new Error('Method not implemented.');
   }
 }
