d5cbc29a43987e25293e8d020eecb3056356f239
Somtime <rghee0821@gmail.com>
2025-08-12 14:58:00 +0900

    Fix: logger middleware 응답 시간 추가

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -11,12 +11,15 @@ export class LoggerMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction): void {
     const { ip, method, originalUrl, body } = request;
     const userAgent = request.get('user-agent') || '';
+    const startHrTime = process.hrtime();
 
     // 응답이 끝났을 때
     response.on('finish', () => {
       const { statusCode } = response;
       const contentLength = response.get('content-length') || 0;
-      let logMessage = `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`;
+      const diff = process.hrtime(startHrTime);
+      const responseTimeMs = Math.round((diff[0] * 1e9 + diff[1]) / 1e6);
+      let logMessage = `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} ${responseTimeMs}ms`;
       if (!isEmpty(body)) {
         logMessage += ` body: ${JSON.stringify(body)}`;
       }
